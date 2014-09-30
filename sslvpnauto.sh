337e4db9b0e0b440d061bf1f2932c48b73d749e3
Alex Fang <frjalex@gmail.com>
2014-09-30 22:15:51 +0800

    Added managing script to /etc/init.d

--- sslvpnauto.sh
+++ sslvpnauto.sh
@@ -67,6 +67,76 @@ wget turin.theredian.org/ocserv.conf
 echo "Counfiguration complete. Now adding 1 user for u. Username:" $username ; read username
 sudo ocpasswd -c /etc/ocserv/ocpasswd $username
 
+#Manage App Script
+cd /etc/init.d
+cat >ocserv <<EOF
+#!/bin/sh
+### BEGIN INIT INFO
+# Provides: ocserv
+# Required-Start: $remote_fs $syslog
+# Required-Stop: $remote_fs $syslog
+# Default-Start: 2 3 4 5
+# Default-Stop: 0 1 6
+### END INIT INFO
+# Copyright Rene Mayrhofer, Gibraltar, 1999
+# This script is distibuted under the GPL
+PATH=/bin:/usr/bin:/sbin:/usr/sbin
+DAEMON=/usr/sbin/ocserv
+PIDFILE=/var/run/ocserv.pid
+DAEMON_ARGS="-c /etc/ocserv/ocserv.conf"
+case "$1" in
+start)
+if [ ! -r $PIDFILE ]; then
+echo -n "Starting OpenConnect VPN Server Daemon: "
+start-stop-daemon --start --quiet --pidfile $PIDFILE --exec $DAEMON -- \
+$DAEMON_ARGS > /dev/null
+echo "ocserv."
+else
+echo -n "OpenConnect VPN Server is already running.\n\r"
+exit 0
+fi
+;;
+stop)
+echo -n "Stopping OpenConnect VPN Server Daemon: "
+start-stop-daemon --stop --quiet --pidfile $PIDFILE --exec $DAEMON
+echo "ocserv."
+rm -f $PIDFILE
+;;
+force-reload|restart)
+echo "Restarting OpenConnect VPN Server: "
+$0 stop
+sleep 1
+$0 start
+;;
+status)
+if [ ! -r $PIDFILE ]; then
+# no pid file, process doesn't seem to be running correctly
+exit 3
+fi
+PID=`cat $PIDFILE | sed 's/ //g'`
+EXE=/proc/$PID/exe
+if [ -x "$EXE" ] &&
+[ "`ls -l \"$EXE\" | cut -d'>' -f2,2 | cut -d' ' -f2,2`" = \
+"$DAEMON" ]; then
+# ok, process seems to be running
+exit 0
+elif [ -r $PIDFILE ]; then
+# process not running, but pidfile exists
+exit 1
+else
+# no lock file to check for, so simply return the stopped status
+exit 3
+fi
+;;
+*)
+echo "Usage: /etc/init.d/ocserv {start|stop|restart|force-reload|status}"
+exit 1
+;;
+esac
+exit 0
+EOF
+
+
 #iptables rules
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A INPUT -d 127.0.0.0/8 -j REJECT
@@ -77,6 +147,9 @@ iptables -A INPUT -p tcp -m tcp --dport 443 -j ACCEPT
 iptables -A INPUT -p tcp -m state --state NEW --dport 22 -j ACCEPT
 iptables -A INPUT -p icmp -j ACCEPT
 iptables -A INPUT -m limit --limit 5/min -j LOG --log-prefix "iptables denied: " --log-level 7
+
+#Start Ocserv
+service ocserv start
 
 clear
 
