d08eb67e0dc5bfde90c2e846c9aa17c0a915eda3
Yang Li <liyangyijie@users.noreply.github.com>
2015-01-20 05:50:40 +0800

    debug

--- ocserv-sysctl.sh
+++ ocserv-sysctl.sh
@@ -9,7 +9,7 @@ gw_intf2=`ip route show | grep '^default' | sed -e 's/.* dev \([^ ]*\).*/\1/'`
 # turn on NAT over default gateway and VPN
 
 if !(iptables-save -t nat | grep -q "$gw_intf2 (ocserv)"); then
-iptables -t nat -A POSTROUTING -s 192.168.10.0/24 -o $gw_intf2 -m comment --comment "$gw_intf2 (ocserv)"-j MASQUERADE
+iptables -t nat -A POSTROUTING -s 192.168.10.0/24 -o $gw_intf2 -m comment --comment "$gw_intf2 (ocserv)" -j MASQUERADE
 fi
 
 iptables -A FORWARD -s 192.168.10.0/24 -j ACCEPT
