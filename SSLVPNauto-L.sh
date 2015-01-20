239bb30f9b95aeca614cbae552ab6159b32c74b5
Yang Li <liyangyijie@users.noreply.github.com>
2015-01-20 23:32:12 +0800

    Update SSLVPNauto-L.sh

--- SSLVPNauto-L.sh
+++ SSLVPNauto-L.sh
@@ -167,34 +167,7 @@ cp server-cert.pem /etc/ocserv/ && cp server-key.pem /etc/ocserv/
 function config_ocserv(){
 cd /etc/ocserv
 wget https://raw.githubusercontent.com/fanyueciyuan/useful/master/ocserv.conf --no-check-certificate
-
-cat << _EOF_ > ocserv-sysctl.sh
-#!/bin/sh
-
-# turn on IP forwarding
-sysctl -w net.ipv4.ip_forward=1
-
-#get gateway
-gw_intf2=`ip route show | grep '^default' | sed -e 's/.* dev \([^ ]*\).*/\1/'`
-
-# turn on NAT over default gateway and VPN
-
-if !(iptables-save -t nat | grep -q "$gw_intf2 (ocserv)"); then
-iptables -t nat -A POSTROUTING -s 192.168.10.0/24 -o $gw_intf2 -m comment --comment "$gw_intf2 (ocserv)" -j MASQUERADE
-fi
-
-iptables -A FORWARD -s 192.168.10.0/24 -j ACCEPT
-
-# turn on MSS fix
-# MSS = MTU - TCP header - IP header
-iptables -t mangle -A FORWARD -p tcp -m tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
-
-#ocserv start
-sudo /usr/sbin/ocserv -c /etc/ocserv/ocserv.conf
-
-echo $0 done
-_EOF_
-
+wget https://raw.githubusercontent.com/fanyueciyuan/useful/master/ocserv-sysctl.sh
 chmod +x ocserv-sysctl.sh
 
 ocserv_port=`cat /etc/ocserv/ocserv.conf | grep '^tcp-port' | sed 's/tcp-port = //g'`
