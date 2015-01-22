a317be4c44d5508c2cca78471bbb42efe1255373
Yang Li <liyangyijie@users.noreply.github.com>
2015-01-22 17:04:01 +0800

    tcp udp ipwrok ipmask from ocserv.conf

--- ocserv-sysctl.sh
+++ ocserv-sysctl.sh
@@ -5,6 +5,11 @@ sysctl -w net.ipv4.ip_forward=1
 
 #get gateway
 gw_intf2=`ip route show | grep '^default' | sed -e 's/.* dev \([^ ]*\).*/\1/'`
+ocserv_tcpport=`cat /etc/ocserv/ocserv.conf | grep '^tcp-port' | sed 's/tcp-port = //g'`
+ocserv_udpport=`cat /etc/ocserv/ocserv.conf | grep '^udp-port' | sed 's/udp-port = //g'`
+ocserv_ip4work=`cat /etc/ocserv/ocserv.conf | grep '^ipv4-network' | sed 's/ipv4-network = //g'`
+ocserv_ip4mask=`cat /etc/ocserv/ocserv.conf | grep '^ipv4-netmask' | sed 's/ipv4-netmask = //g'`
+
 
 # turn on NAT over default gateway and VPN
 
@@ -12,7 +17,9 @@ if !(iptables-save -t nat | grep -q "$gw_intf2 (ocserv)"); then
 iptables -t nat -A POSTROUTING -s 192.168.10.0/24 -o $gw_intf2 -m comment --comment "$gw_intf2 (ocserv)" -j MASQUERADE
 fi
 
-iptables -A FORWARD -s 192.168.10.0/24 -j ACCEPT
+iptables -A FORWARD -s $ocserv_ip4work/$ocserv_ip4mask -j ACCEPT
+iptables -A INPUT -p tcp --dport $ocserv_tcpport -j ACCEPT
+iptables -A INPUT -p udp --dport $ocserv_udpport -j ACCEPT
 
 # turn on MSS fix
 # MSS = MTU - TCP header - IP header
