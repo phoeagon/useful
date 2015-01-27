c94ba299aad1b2ada1f9089d1bee174bcd2ee698
Yang Li <liyangyijie@users.noreply.github.com>
2015-01-27 09:29:55 +0800

    Update SSLVPNauto-L-0.2.sh

--- ocservauto/SSLVPNauto-L-0.2.sh
+++ ocservauto/SSLVPNauto-L-0.2.sh
@@ -143,17 +143,20 @@ function check_Required {
     print_info "sources ok"
     clear
 }
+
 #error and force-exit
 function die {
     echo "ERROR: $1" > /dev/null 1>&2
     exit 1
 }
+
 #ok echo
 function print_info {
     echo -n -e '\e[1;36m'
     echo -n $1
     echo -e '\e[0m'
 }
+
 #warn echo
 function print_warn {
     echo -n -e '\033[41;37m'
@@ -261,6 +264,8 @@ function get_Custom_configuration(){
     fi
     echo "####################################"
 }
+
+#add a user 增加一个初始用户
 function add_a_user(){
 # Get username
     print_info "Input your username for ocserv:"
@@ -278,20 +283,18 @@ function add_a_user(){
     fi
     print_info "Your password:$password"
     echo "####################################"
-
 }
 
-# pre_install
+# install dependencies 安装依赖文件
 function pre_install(){
-   #keep kernel 防止某些情况下内核升级
-   echo linux-image-`uname -r` hold | sudo dpkg --set-selections
-   apt-get upgrade -y
-   
-#   #no update from test sources 将测试源优先级调低 防止其他测试包安装
-#  if [ ! -d /etc/apt/preferences.d ];then
-#       mkdir /etc/apt/preferences.d
-#  fi
-#  cat > /etc/apt/preferences.d/my_ocserv_preferences<<EOF
+#keep kernel 防止某些情况下内核升级
+    echo linux-image-`uname -r` hold | sudo dpkg --set-selections
+    apt-get upgrade -y
+##no update from test sources 将测试源优先级调低 防止其他测试包安装
+#    if [ ! -d /etc/apt/preferences.d ];then
+#        mkdir /etc/apt/preferences.d
+#    fi
+#    cat > /etc/apt/preferences.d/my_ocserv_preferences<<EOF
 #Package: *
 #Pin: release wheezy
 #Pin-Priority: 900
@@ -302,116 +305,98 @@ function pre_install(){
 #Pin: release jessie
 #Pin-Priority: 60
 #EOF
- 
-   #sources check, Do not change the order 不要轻易改变升级顺序
-   cat /etc/apt/sources.list | grep 'deb http://ftp.debian.org/debian wheezy-backports main contrib non-free' > /dev/null 2>&1
-   if [ $? -ne 0 ]; then
-   echo "deb http://ftp.debian.org/debian wheezy-backports main contrib non-free" >> /etc/apt/sources.list
-   oc_wheezy_backports="n"
-   fi
-   
-   apt-get update   
-   
-   apt-get install -y libprotobuf-c0-dev 
-   apt-get install -y libreadline6 libreadline5 libreadline6-dev libgmp3-dev m4 gcc pkg-config make gnutls-bin libtalloc-dev build-essential libwrap0-dev libpam0g-dev libdbus-1-dev libreadline-dev libnl-route-3-dev libpcl1-dev libopts25-dev autogen  libnl-nf-3-dev debhelper libseccomp-dev
-   apt-get install -y -t wheezy-backports  libgnutls28-dev 
-   
-   #sources check @ check Required
-
-   echo "deb ftp://ftp.debian.org/debian/ jessie main contrib non-free" >> /etc/apt/sources.list 
-  
-   
-   #update dependencies too new ~
-   #apt-get update
-   #apt-get install -y -t jessie  libprotobuf-c-dev libhttp-parser-dev
-   #apt-get install -y -qq -t jessie  libprotobuf-c-dev libhttp-parser-dev
-   
-   #if sources del
-   if [ "$oc_wheezy_backports" = "n" ]; then
-   sed -i 's@deb http://ftp.debian.org/debian wheezy-backports main contrib non-free@@g' /etc/apt/sources.list
-   fi
-   
-   if [ "$oc_jessie" = "n" ]; then
-   sed -i 's@deb ftp://ftp.debian.org/debian/ jessie main contrib non-free@@g' /etc/apt/sources.list
-   fi
-   
-   #keep update
-#   rm -rf /etc/apt/preferences.d/my_ocserv_preferences
-   apt-get update
-   
-   print_info "dependencies  ok"
+#sources check, Do not change the order 不要轻易改变升级顺序
+    cat /etc/apt/sources.list | grep 'deb http://ftp.debian.org/debian wheezy-backports main contrib non-free' > /dev/null 2>&1
+    if [ $? -ne 0 ]; then
+       echo "deb http://ftp.debian.org/debian wheezy-backports main contrib non-free" >> /etc/apt/sources.list
+       oc_wheezy_backports="n"
+    fi
+    apt-get update
+    apt-get install -y libprotobuf-c0-dev 
+    apt-get install -y libreadline6 libreadline5 libreadline6-dev libgmp3-dev m4 gcc pkg-config make gnutls-bin libtalloc-dev build-essential libwrap0-dev libpam0g-dev libdbus-1-dev libreadline-dev libnl-route-3-dev libpcl1-dev libopts25-dev autogen  libnl-nf-3-dev debhelper libseccomp-dev
+    apt-get install -y -t wheezy-backports  libgnutls28-dev 
+#sources check @ check Required 源检测在前面
+    echo "deb ftp://ftp.debian.org/debian/ jessie main contrib non-free" >> /etc/apt/sources.list
+##update dependencies too new ~
+#apt-get update
+#apt-get install -y -t jessie  libprotobuf-c-dev libhttp-parser-dev
+#apt-get install -y -qq -t jessie  libprotobuf-c-dev libhttp-parser-dev
+#if sources del 如果本来没有测试源便删除
+    if [ "$oc_wheezy_backports" = "n" ]; then
+        sed -i 's@deb http://ftp.debian.org/debian wheezy-backports main contrib non-free@@g' /etc/apt/sources.list
+    fi
+    if [ "$oc_jessie" = "n" ]; then
+        sed -i 's@deb ftp://ftp.debian.org/debian/ jessie main contrib non-free@@g' /etc/apt/sources.list
+    fi
+#keep update
+#    rm -rf /etc/apt/preferences.d/my_ocserv_preferences
+    apt-get update
+    print_info "dependencies  ok"
 }
-# install
+# install 编译安装
 function tar_ocserv_install(){
-   cd ~
+    cd ~
 #max router rulers
-   if [ "$max_router" = "" ]; then
+    if [ "$max_router" = "" ]; then
         max_router="200"
-   fi
-   wget ftp://ftp.infradead.org/pub/ocserv/ocserv-0.8.9.tar.xz
-   tar xvf ocserv-0.8.9.tar.xz
-   rm -rf ocserv-0.8.9.tar.xz
-   cd ocserv-0.8.9
+    fi
+    wget ftp://ftp.infradead.org/pub/ocserv/ocserv-0.8.9.tar.xz
+    tar xvf ocserv-0.8.9.tar.xz
+    rm -rf ocserv-0.8.9.tar.xz
+    cd ocserv-0.8.9
 #have to use "" then $ work ,set router limit
-   sed -i "s/#define MAX_CONFIG_ENTRIES 64/#define MAX_CONFIG_ENTRIES $max_router/g" src/vpn.h
-   ./configure --prefix=/usr --sysconfdir=/etc && make && make install
-   
+    sed -i "s/#define MAX_CONFIG_ENTRIES 64/#define MAX_CONFIG_ENTRIES $max_router/g" src/vpn.h
+    ./configure --prefix=/usr --sysconfdir=/etc && make && make install
 #check install 检测编译安装是否成功
-   if [ ! -f /usr/sbin/ocserv ]
-   then
-	    die "ocserv install failure,check dependencies!"
-   fi
-   
-   mkdir -p /etc/ocserv/CAforOC
-   cp doc/profile.xml /etc/ocserv
-   cp doc/dbus/org.infradead.ocserv.conf /etc/dbus-1/system.d/
-   sed -i "s@localhost@$ocserv_hostname@g" /etc/ocserv/profile.xml
-   cd ..
-   rm -rf ocserv-0.8.9
-   
+    if [ ! -f /usr/sbin/ocserv ]
+    then
+        die "ocserv install failure,check dependencies!"
+    fi
+#mv files
+    mkdir -p /etc/ocserv/CAforOC
+    cp doc/profile.xml /etc/ocserv
+    cp doc/dbus/org.infradead.ocserv.conf /etc/dbus-1/system.d/
+    sed -i "s@localhost@$ocserv_hostname@g" /etc/ocserv/profile.xml
+    cd ..
+    rm -rf ocserv-0.8.9
 # get config file from net
-  
-   cd /etc/ocserv
-   wget https://raw.githubusercontent.com/fanyueciyuan/useful/master/ocservauto/ocserv.conf --no-check-certificate
-   wget https://raw.githubusercontent.com/fanyueciyuan/useful/master/ocservauto/start-ocserv-sysctl.sh  --no-check-certificate
-   wget https://raw.githubusercontent.com/fanyueciyuan/useful/master/ocservauto/stop-ocserv-sysctl.sh  --no-check-certificate
-   wget https://raw.githubusercontent.com/fanyueciyuan/useful/master/ocservauto/ocserv  --no-check-certificate
-   chmod 755 ocserv
-   mv ocserv /etc/init.d
-   chmod +x start-ocserv-sysctl.sh
-   chmod +x stop-ocserv-sysctl.sh
-   touch ocpasswd
-   chmod 600 ocpasswd   
- 
-   print_info "ocserv install ok"
+    cd /etc/ocserv
+    wget https://raw.githubusercontent.com/fanyueciyuan/useful/master/ocservauto/ocserv.conf --no-check-certificate
+    wget https://raw.githubusercontent.com/fanyueciyuan/useful/master/ocservauto/start-ocserv-sysctl.sh  --no-check-certificate
+    wget https://raw.githubusercontent.com/fanyueciyuan/useful/master/ocservauto/stop-ocserv-sysctl.sh  --no-check-certificate
+    wget https://raw.githubusercontent.com/fanyueciyuan/useful/master/ocservauto/ocserv  --no-check-certificate
+    chmod 755 ocserv
+    mv ocserv /etc/init.d
+    chmod +x start-ocserv-sysctl.sh
+    chmod +x stop-ocserv-sysctl.sh
+    touch ocpasswd
+    chmod 600 ocpasswd   
+    print_info "ocserv install ok"
 }
 
 function make_ocserv_ca(){
 #all in one doc
-cd /etc/ocserv/CAforOC
-
+    cd /etc/ocserv/CAforOC
 # Self-signed CA set
 #CA's name
-if [ "$caname" = "" ]; then
+    if [ "$caname" = "" ]; then
         caname="ocvpn"
-fi
+    fi
 #Organization name
-if [ "$ogname" = "" ]; then
+    if [ "$ogname" = "" ]; then
         ogname="ocvpn"
-fi
+    fi
 #Company name
-if [ "$oname" = "" ]; then
+    if [ "$oname" = "" ]; then
         oname="ocvpn"
-fi
+    fi
 #server's FQDN
-if [ "$fqdnname" = "" ]; then
+    if [ "$fqdnname" = "" ]; then
         fqdnname=$ocserv_hostname
-fi
-
-
+    fi
 #server-ca
-certtool --generate-privkey --outfile ca-key.pem
-cat << _EOF_ > ca.tmpl
+    certtool --generate-privkey --outfile ca-key.pem
+    cat << _EOF_ > ca.tmpl
 cn = "$caname"
 organization = "$ogname"
 serial = 1
@@ -421,12 +406,10 @@ signing_key
 cert_signing_key
 crl_signing_key
 _EOF_
-
-certtool --generate-self-signed --load-privkey ca-key.pem --template ca.tmpl --outfile ca-cert.pem
-
+    certtool --generate-self-signed --load-privkey ca-key.pem --template ca.tmpl --outfile ca-cert.pem
 #server-key
-certtool --generate-privkey --outfile server-key.pem
-cat << _EOF_ > server.tmpl
+    certtool --generate-privkey --outfile server-key.pem
+    cat << _EOF_ > server.tmpl
 cn = "$fqdnname"
 organization = "$oname"
 serial = 2
@@ -435,119 +418,102 @@ signing_key
 encryption_key
 tls_www_server
 _EOF_
-
-certtool --generate-certificate --load-privkey server-key.pem --load-ca-certificate ca-cert.pem --load-ca-privkey ca-key.pem --template server.tmpl --outfile server-cert.pem
-
-if [ ! -f server-cert.pem ] || [ ! -f server-key.pem ]; then	
-	die "CA or KEY NOT Found , make failure!"
-fi
-
-cp server-cert.pem /etc/ocserv/ && cp server-key.pem /etc/ocserv/
-   
-print_info "Self-signed CA for ocserv ok"
-
+    certtool --generate-certificate --load-privkey server-key.pem --load-ca-certificate ca-cert.pem --load-ca-privkey ca-key.pem --template server.tmpl --outfile server-cert.pem
+    if [ ! -f server-cert.pem ] || [ ! -f server-key.pem ]; then	
+        die "CA or KEY NOT Found , make failure!"
+    fi
+    cp server-cert.pem /etc/ocserv/ && cp server-key.pem /etc/ocserv/
+    print_info "Self-signed CA for ocserv ok"
 }
 
 function ca_login_ocserv(){
-
-print_warn "CA_Login DO NOT support"
-
+    print_warn "CA_Login DO NOT support"
 }
-# set
+
+# set 设定相关参数
 function set_ocserv_conf(){
-
-#set port 
-  if [ "$ocserv_tcpport_set" != "" ]; then
-    sed -i "s@tcp-port = $ocserv_tcpport_Default@tcp-port = $ocserv_tcpport_set@g" /etc/ocserv/ocserv.conf
-  fi
-
-  if [ "$ocserv_udpport_set" != "" ]; then
-    sed -i "s@udp-port = $ocserv_udpport_Default@udp-port = $ocserv_udpport_set@g" /etc/ocserv/ocserv.conf
-  fi
-  
+#set port
+    if [ "$ocserv_tcpport_set" != "" ]; then
+        sed -i "s@tcp-port = $ocserv_tcpport_Default@tcp-port = $ocserv_tcpport_set@g" /etc/ocserv/ocserv.conf
+    fi
+    if [ "$ocserv_udpport_set" != "" ]; then
+        sed -i "s@udp-port = $ocserv_udpport_Default@udp-port = $ocserv_udpport_set@g" /etc/ocserv/ocserv.conf
+    fi
 #default domain 
-sed -i "s@#default-domain = example.com@default-domain = $fqdnname@" /etc/ocserv/ocserv.conf 
-  
+    sed -i "s@#default-domain = example.com@default-domain = $fqdnname@" /etc/ocserv/ocserv.conf 
 #Boot from the start 开机自启
-if [ "$ocserv_boot_start" = "" ]; then
-sudo insserv ocserv
-fi
-
+    if [ "$ocserv_boot_start" = "" ]; then
+        sudo insserv ocserv
+    fi
 #add a user
-(echo "$password"; sleep 1; echo "$password") | ocpasswd -c "/etc/ocserv/ocpasswd" $username
+    (echo "$password"; sleep 1; echo "$password") | ocpasswd -c "/etc/ocserv/ocpasswd" $username
 
 #set ca_login
-  if [ "$ca_login" = "y" ]; then
-    sed -i "s@auth = "plain[/etc/ocserv/ocpasswd]"@#auth = "plain[/etc/ocserv/ocpasswd]"@g" /etc/ocserv/ocserv.conf
-	sed -i "s@#auth = "certificate"@auth = "certificate"@" /etc/ocserv/ocserv.conf
-  fi
-  
-print_info "set ocserv ok"
-  
+    if [ "$ca_login" = "y" ]; then
+        sed -i "s@auth = "plain[/etc/ocserv/ocpasswd]"@#auth = "plain[/etc/ocserv/ocpasswd]"@g" /etc/ocserv/ocserv.conf
+        sed -i "s@#auth = "certificate"@auth = "certificate"@" /etc/ocserv/ocserv.conf
+    fi
+    print_info "set ocserv ok"
 }
+
 function stop_ocserv(){
 #stop all
-oc_pid=`pidof ocserv`
-if [ ! -z "$oc_pid" ]; then
+    oc_pid=`pidof ocserv`
+    if [ ! -z "$oc_pid" ]; then
         for pid in $oc_pid
         do
             kill -9 $pid > /dev/null 2>&1
             if [ $? -eq 0 ]; then
-            echo "ocserv process[$pid] has been killed"
+                echo "ocserv process[$pid] has been killed"
             fi
         done
-fi
+    fi
 }
+
 function start_ocserv(){
-if [ ! -f /etc/ocserv/server-cert.pem ] || [ ! -f /etc/ocserv/server-key.pem ]; then
-	print_warn "You have to put your CA and Key to /etc/ocserv !!!"
-	print_warn "You have to change your CA and Key'name to server-cert.pem and server-key.pem !!!"
-	die "CA or KEY NOT Found !!!"
-fi
-
+    if [ ! -f /etc/ocserv/server-cert.pem ] || [ ! -f /etc/ocserv/server-key.pem ]; then
+        print_warn "You have to put your CA and Key to /etc/ocserv !!!"
+        print_warn "You have to change your CA and Key'name to server-cert.pem and server-key.pem !!!"
+        die "CA or KEY NOT Found !!!"
+    fi
 #start
-
-/etc/init.d/ocserv start
+    /etc/init.d/ocserv start
 }
+
 function show_ocserv(){
-ocserv_port=`cat /etc/ocserv/ocserv.conf | grep '^tcp-port' | sed 's/tcp-port = //g'`
-clear
-
-ps -ef | grep -v grep | grep -v ps | grep -i '/usr/sbin/ocserv' > /dev/null 2>&1
-
-if [ $? -eq 0 ]; then
-    if [ "$ca_login" = "y" ]; then
-	
-	echo "test!"
-	
-	else
-	echo ""
-    echo -e "\033[41;37m Your server domain is \033[0m" "$fqdnname:$ocserv_port"
-    echo -e "\033[41;37m Your username is \033[0m" "$username"
-    echo -e "\033[41;37m Your password is \033[0m" "$password"
-    print_warn " You can use ' sudo ocpasswd -c /etc/ocserv/ocpasswd username ' to add users. "
-    print_warn " You can stop ocserv by ' /etc/init.d/ocserv stop ' !"
-    print_warn " Boot from the start or not, use ' sudo insserv ocserv ' or ' sudo insserv -r ocserv ' ."
-    echo ""    
-    print_info "enjoy it!"
-    echo ""
-	fi
-elif [ "$self_signed_ca" = "n" -a "$ca_login" = "" ]; then    
-	print_warn "1,You have to change your CA and Key'name to server-cert.pem and server-key.pem !!!"
-	print_warn "2,You have to put your CA and Key to /etc/ocserv !!!"
-	print_warn "3,You have to start ocserv by '/etc/init.d/ocserv start'!"
-	print_warn "4,You can use ' sudo ocpasswd -c /etc/ocserv/ocpasswd username ' to add users."
-	print_warn "5,Boot from the start or not, use ' sudo insserv ocserv ' or ' sudo insserv -r ocserv ' ."
-	echo -e "\033[41;37m Your username is \033[0m" "$username"
-	echo -e "\033[41;37m Your password is \033[0m" "$password"
-	
-elif [ "$self_signed_ca" = "n" -a "$ca_login" = "y" ]; then  
-    echo "test 2"
-else
-    print_warn "ocserv start failure,ocserv is offline!"	
-fi
-
-
+    ocserv_port=`cat /etc/ocserv/ocserv.conf | grep '^tcp-port' | sed 's/tcp-port = //g'`
+    clear
+    ps -ef | grep -v grep | grep -v ps | grep -i '/usr/sbin/ocserv' > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        if [ "$ca_login" = "y" ]; then
+            echo "test!"
+            echo "test!"
+        else
+            echo ""
+            echo -e "\033[41;37m Your server domain is \033[0m" "$fqdnname:$ocserv_port"
+            echo -e "\033[41;37m Your username is \033[0m" "$username"
+            echo -e "\033[41;37m Your password is \033[0m" "$password"
+            print_warn " You can use ' sudo ocpasswd -c /etc/ocserv/ocpasswd username ' to add users. "
+            print_warn " You can stop ocserv by ' /etc/init.d/ocserv stop ' !"
+            print_warn " Boot from the start or not, use ' sudo insserv ocserv ' or ' sudo insserv -r ocserv ' ."
+            echo ""    
+            print_info "enjoy it!"
+            echo ""
+        fi
+    elif [ "$self_signed_ca" = "n" -a "$ca_login" = "" ]; then    
+        print_warn "1,You have to change your CA and Key'name to server-cert.pem and server-key.pem !!!"
+        print_warn "2,You have to put your CA and Key to /etc/ocserv !!!"
+        print_warn "3,You have to start ocserv by '/etc/init.d/ocserv start'!"
+        print_warn "4,You can use ' sudo ocpasswd -c /etc/ocserv/ocpasswd username ' to add users."
+        print_warn "5,Boot from the start or not, use ' sudo insserv ocserv ' or ' sudo insserv -r ocserv ' ."
+        echo -e "\033[41;37m Your username is \033[0m" "$username"
+        echo -e "\033[41;37m Your password is \033[0m" "$password"
+    elif [ "$self_signed_ca" = "n" -a "$ca_login" = "y" ]; then  
+        echo "test 2"
+        echo "test 2"
+    else
+        print_warn "ocserv start failure,ocserv is offline!"	
+    fi
 }
 
 # Initialization step
