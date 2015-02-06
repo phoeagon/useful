531d6bec7d663a64569659ca581811959d1458e1
Yang Li <liyangyijie@users.noreply.github.com>
2015-02-06 15:47:39 +0800

    Update stop-ocserv-sysctl.sh

--- ocservauto/stop-ocserv-sysctl.sh
+++ ocservauto/stop-ocserv-sysctl.sh
@@ -5,13 +5,8 @@
 # sysctl -w net.ipv4.ip_forward=0
 
 #del iptables
-cat << _EOF_ > /etc/ocserv/temp.sh
-#! /bin/bash
 
-_EOF_
-iptables-save | grep 'ocserv' | sed 's/^-A P/iptables -t nat -D P/' | sed 's/^-A FORWARD -p/iptables -t mangle -D FORWARD -p/' | sed 's/^-A/iptables -D/' >> /etc/ocserv/temp.sh
-chmod 755 /etc/ocserv/temp.sh
-bash /etc/ocserv/temp.sh
-rm -rf /etc/ocserv/temp.sh
+iptables-save | grep 'ocserv' | sed 's/^-A P/iptables -t nat -D P/' | sed 's/^-A FORWARD -p/iptables -t mangle -D FORWARD -p/' | sed 's/^-A/iptables -D/' | bash
+
 
 echo "..."
